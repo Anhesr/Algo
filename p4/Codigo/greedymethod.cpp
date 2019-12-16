6fbe601ab35237a1c39b4f809fe429c56e7321ff
Angel Santos Ramirez <i72saraf@uco.es>
2019-12-16 17:39:29 +0100

    1

--- p4/Codigo/greedymethod.cpp
+++ p4/Codigo/greedymethod.cpp
@@ -67,20 +67,19 @@ void Algorithm::calculatePolygonalApproximationGreedy(int myN){
       long double error= 1000000000000000.0;
       int pos;
 
-      for (int i = 1; i < trueDominantPointPosition.size() - 1 ;i++)
-      {
-
-        for(int j = trueDominantPointPosition[i-1]+1; j < trueDominantPointPosition[i+1];j++){
-
-          if(error > calculateISEValue(trueDominantPointPosition[i-1], j ) + calculateISEValue(j, trueDominantPointPosition[i+1]) ){
-
-            error=calculateISEValue(trueDominantPointPosition[i-1], j ) + calculateISEValue(j, trueDominantPointPosition[i+1]);
-            
-            trueDominantPointPosition[i]=j;
-          }
+      for (int i = 0; i < trueDominantPointPosition.size()-2; ++i) // HASTA N-2 PARA NO HACER VIOLACION DE SEGMENTO EN AUX2 DE ABAJO
+    {
+      int aux1 = trueDominantPointPosition[i];
+      int aux2 = trueDominantPointPosition[i+2]; // COGO LOS DOS PUNTOS DOMINANTES DE LOS EXTREMOS DE LA COMPARACION
+      error = abs(calculateISEValue(aux1, trueDominantPointPosition[i+1])) + abs(calculateISEValue(trueDominantPointPosition[i+1], aux2));
+      for (int j = aux1+1; j < aux2; ++j) // Y DOY A ERROR EL VALOR ACTUAL DE LOS PUNTOS DOMINANTES
+      { // VOY COMPARANDO CON TODOS LOS PUNTOS INTERMEDIOS DE i e i+2 EL ERROR
+        if(abs(error) > abs(calculateISEValue(aux1, j)) + abs(calculateISEValue(j, aux2))){
+          error = abs(calculateISEValue(aux1, j)) + abs(calculateISEValue(j, aux2)); // SI ES MENOR ACTUALIZO EL ERROR
+          trueDominantPointPosition[i+1] = j; // Y ACTUALIZO EL PUNTO DOMINANTE
         }
-        error= 1000000000000000000000.0;
       }
+    }
 
       setDominantPointsPosition(trueDominantPointPosition);
 
